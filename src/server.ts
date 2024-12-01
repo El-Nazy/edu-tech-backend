521a214b95b892adfb0806bb640dbdb98c73d1ae
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2024-12-01 23:43:07 +0100

    feat: port payload's default /login endpoint to /sign-in

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ dotenv.config({
 
 import express from 'express'
 import payload from 'payload'
+import Users from './payload/collections/Users'
+import { PayloadRequest } from 'payload/types'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -18,6 +20,34 @@ app.get('/', (_, res) => {
 const start = async (): Promise<void> => {
   await payload.init({
     express: app,
+    onInit: (payload) => {
+      const app = payload.express
+
+      // Redirect API login endpoint
+      app.post('/api/v1/users/sign-in', async (req, res, next) => {
+        // Manually forward the request to the default login route
+
+        // Call the default Payload login route handler directly
+        try {
+          const loginResponse = await payload.login({
+            collection: Users.slug as 'users',
+            req: req as PayloadRequest,
+            data: req.body,
+            res: res,
+          })
+          res.json(loginResponse) // Respond with the login result
+        } catch (error) {
+          res.status(401).json({
+            errors: [
+              {
+                message: error.message,
+              },
+            ],
+          })
+          next(error) // If any error occurs during login, pass it to the error handler
+        }
+      })
+    },
     secret: process.env.PAYLOAD_SECRET!,
   })
 
