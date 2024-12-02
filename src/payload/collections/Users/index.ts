dd8176e7f6b49bb648afe39ccc82ad4b7c451dd1
Emmanuel Ngene <emmanuelchinazangene@gmail.com>
2024-12-02 01:02:19 +0100

    feat: finish all initial user auth setup before email verification

--- src/payload/collections/Users/index.ts
+++ src/payload/collections/Users/index.ts
@@ -1,4 +1,4 @@
-import type { CollectionConfig } from 'payload/types'
+import type { CollectionConfig, PayloadRequest } from 'payload/types'
 
 import { email as validateEmail } from 'payload/dist/fields/validations'
 
@@ -9,6 +9,8 @@ import { ensureFirstUserIsAdmin } from './hooks/ensureFirstUserIsAdmin'
 import { loginAfterCreate } from './hooks/loginAfterCreate'
 import { sanitizeDemoAdmin } from './hooks/sanitizeDemoAdmin'
 import { addMinutes } from 'date-fns'
+import { User } from '../../payload-types'
+import payload from 'payload'
 
 const Users: CollectionConfig = {
   access: {
@@ -19,7 +21,36 @@ const Users: CollectionConfig = {
   //   defaultColumns: ['name', 'email'],
   //   useAsTitle: 'name',
   // },
-  auth: true,
+  auth: {
+    forgotPassword: {
+      generateEmailHTML: ({
+        req,
+        token,
+        user,
+      }: {
+        req: PayloadRequest
+        token: string
+        user: User
+      }) => {
+        // Use the token provided to allow your user to reset their password
+        payload.logger.info(`req body ${JSON.stringify(req.body)}`, req.body)
+        const resetPasswordURL = `${req.body.redirect || 'https://localhost:3000/reset-password'}?token=${token}`
+
+        return `
+        <!doctype html>
+        <html>
+          <body>
+            <p>Hello, ${(user.firstName && user.lastName && user.firstName + ' ' + user.lastName) || user.firstName || user.lastName || user.email}</p>
+            <p>Click below to reset your password.</p>
+            <p>
+              <a href="${resetPasswordURL}">${resetPasswordURL}</a>
+            </p>
+          </body>
+        </html>
+      `
+      },
+    },
+  },
   fields: [
     {
       name: 'emailVerified',
@@ -29,19 +60,71 @@ const Users: CollectionConfig = {
     {
       name: 'emailVerificationHash',
       type: 'text',
-      defaultValue: 'test def val',
       hidden: true,
+      access: {
+        read: () => false,
+      },
     },
     {
       name: 'emailVerificationExpiresAt',
       type: 'date',
-      defaultValue: () => addMinutes(new Date(), 1),
+      // defaultValue: () => addMinutes(new Date(), 1),
       hidden: true,
+      access: {
+        read: () => false,
+      },
     },
     {
-      name: 'name',
+      name: 'picture',
       type: 'text',
     },
+    {
+      name: 'firstName',
+      type: 'text',
+    },
+    {
+      name: 'lastName',
+      type: 'text',
+    },
+    {
+      name: 'countryOfResidence',
+      type: 'text',
+    },
+    {
+      name: 'title',
+      type: 'text',
+    },
+    {
+      name: 'company',
+      type: 'text',
+    },
+    {
+      name: 'specialisation',
+      type: 'text',
+    },
+    {
+      name: 'level',
+      type: 'text',
+    },
+    {
+      name: 'interests',
+      type: 'text',
+    },
+    {
+      name: 'bio',
+      type: 'text',
+    },
+    /**
+picture
+countryOfResidence
+title
+company
+specialisation
+level
+interests
+bio
+
+     */
     // {
     //   // override default email field to add a custom validate function to prevent users from changing the login email
     //   name: 'email',
